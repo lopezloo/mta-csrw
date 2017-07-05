1ac0543bcb34a446591f19c6defb8e8e646d25ce
lopezloo <lopez@multitheftauto.pl>
2017-07-05 10:37:15 +0200

    Fix nil with number comparision

--- csrw/shared/utils/utils.lua
+++ csrw/shared/utils/utils.lua
@@ -166,37 +166,6 @@ function getPlayerMoneyEx(player)
 	return getElementData(player, "money")
 end
 
-if not localPlayer and tonumber(getVersion().mta) < 1.4 then -- od 1.4 po stronie serwera
-	function getElementMatrix(element)
-		local rx, ry, rz = getElementRotation(element, "ZXY")
-		rx, ry, rz = math.rad(rx), math.rad(ry), math.rad(rz)
-		local matrix = {}
-		matrix[1] = {}
-		matrix[1][1] = math.cos(rz)*math.cos(ry) - math.sin(rz)*math.sin(rx)*math.sin(ry)
-		matrix[1][2] = math.cos(ry)*math.sin(rz) + math.cos(rz)*math.sin(rx)*math.sin(ry)
-		matrix[1][3] = -math.cos(rx)*math.sin(ry)
-		matrix[1][4] = 0
-
-		matrix[2] = {}
-		matrix[2][1] = -math.cos(rx)*math.sin(rz)
-		matrix[2][2] = math.cos(rz)*math.cos(rx)
-		matrix[2][3] = math.sin(rx)
-		matrix[2][4] = 0
-
-		matrix[3] = {}
-		matrix[3][1] = math.cos(rz)*math.sin(ry) + math.cos(ry)*math.sin(rz)*math.sin(rx)
-		matrix[3][2] = math.sin(rz)*math.sin(ry) - math.cos(rz)*math.cos(ry)*math.sin(rx)
-		matrix[3][3] = math.cos(rx)*math.cos(ry)
-		matrix[3][4] = 0
-
-		matrix[4] = {}
-		matrix[4][1], matrix[4][2], matrix[4][3] = getElementPosition(element)
-		matrix[4][4] = 1
-
-		return matrix
-	end
-end
-
 function getPositionFromElementOffset(element,offX,offY,offZ)
 	local m = getElementMatrix ( element )  -- Get the matrix
 	local x = offX * m[1][1] + offY * m[2][1] + offZ * m[3][1] + m[4][1]  -- Apply transform
